6a8a029b6b9c452ba75f856be3884e3ff0604da8
Tiago Barbosa <tiago@farias.cc>
2023-07-03 18:16:41 -0300

    initial commit - pratice tests using xUnit.

---
+++ Estacionamento-Tests/VehicleTests.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics.CodeAnalysis;
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+
+namespace Estacionamento_Tests;
+
+public class VehicleTests
+{
+    private Veiculo _veiculoTest = new();
+    
+    [Fact(DisplayName = "Testing vehicle acelerate")]
+    public void TestVehicleAcelerate()
+    {
+        // Arrange
+        // var vehicle = new Veiculo();
+        // Act
+        _veiculoTest.Acelerar(10);
+        // Assert
+        Assert.Equal(100, _veiculoTest.VelocidadeAtual);
+    }
+
+    [Fact(DisplayName = "Testing vehicle brake")]
+    public void TestVehicleBrake()
+    {
+        // Arrange
+        // var vehicle = new Veiculo();
+        // Act
+        _veiculoTest.Frear(10);
+        // Assert
+        Assert.Equal(-150, _veiculoTest.VelocidadeAtual);
+    }
+
+
+    [Fact(DisplayName = "Testing vehicle type")]
+    public void TestTypeVehicle()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Equal(TipoVeiculo.Automovel, _veiculoTest.Tipo);
+    }
+}
